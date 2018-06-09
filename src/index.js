ce23db257a622afcd9ceacbe3e0f0f7fe7490a43
Luis Thomas <thomasluis555@gmail.com>
2018-06-09 18:31:42 -0400

    apollo-boost react-apollo graphql-tag graphql were added as dependencies. removed files. added apollo client and apollo provider to app entry point at index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const client = new ApolloClient({
+    uri: "http://localhost:4000/"
+});
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider >, document.getElementById('root'));
 registerServiceWorker();
