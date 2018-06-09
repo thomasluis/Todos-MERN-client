87048396d6db835963909cb3571e3e3b23d800ab
Luis Thomas <thomasluis555@gmail.com>
2018-06-09 18:42:56 -0400

    now loading data from backend using graphql query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
+const TodosQuery = gql`
+{
+  todos {
+    id 
+    text
+    complete
+  }
+}
+`
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <p>Hello</p>        
-      </div>
-    );
+    const { data: {loading, todos} } = this.props;
+    console.log("data", todos, "loading", loading);
+    if(loading) {
+      return null;
+    }
+    
+  return <div>{todos.map(todo => (<div key={todo.id}>{todo.text}</div>))}</div>
   }
 }
 
-export default App;
+export default graphql(TodosQuery)(App);
